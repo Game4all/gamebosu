2f7c8aeb0e797ca4357cbaeb18287d86ee82bb8e
Lucas A <game4allyt@gmail.com>
2020-04-16 20:16:46 +0200

    Add DrawableGameboyScreen.

---
+++ osu.Game.Rulesets.Gamebosu.Tests/TestSceneGameboyScreen.cs
@@ -0,0 +1,22 @@
+﻿using osu.Game.Rulesets.Gamebosu.UI.Gameboy;
+using osu.Game.Tests.Visual;
+
+namespace osu.Game.Rulesets.Gamebosu.Tests
+{
+    public class TestSceneGameboyScreen : OsuTestScene
+    {
+        private readonly DrawableGameboyScreen screen;
+
+        public TestSceneGameboyScreen()
+        {
+            Child = screen = new DrawableGameboyScreen
+            {
+                Size = new osuTK.Vector2(160, 144),
+                Anchor = Framework.Graphics.Anchor.Centre,
+                Origin = Framework.Graphics.Anchor.Centre
+            };
+
+            screen.Clear();
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Gamebosu/UI/Gameboy/DrawableGameboyScreen.cs
@@ -0,0 +1,49 @@
+﻿using Emux.GameBoy.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace osu.Game.Rulesets.Gamebosu.UI.Gameboy
+{
+    /// <summary>
+    /// The drawable screen of a gameboy emulator.
+    /// </summary>
+    //TODO: investigate whether it would be benefical to make this use a custom draw node instead of uploading textures every 1/60 of a second.
+    public class DrawableGameboyScreen : Sprite, IVideoOutput
+    {
+        private byte[] screenData;
+
+        public DrawableGameboyScreen()
+        {
+            Texture = new Texture(160, 144);
+            screenData = new byte[160 * 144 * sizeof(int)]; //since the 4 components of a color (r, g, b a) are each a byte (4 bytes in total), the same as an int.
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < screenData.Length; i++)
+                screenData[i] = byte.MaxValue;
+
+            uploadTex();
+        }
+
+        public void RenderFrame(byte[] pixelData)
+        {
+            for (int i = 0, j = 0; j < pixelData.Length; i += 4, j += 3)
+            {
+                screenData[i] = pixelData[j]; //r component
+                screenData[i + 1] = pixelData[j + 1]; //g component
+                screenData[i + 2] = pixelData[j + 2]; //b component
+                screenData[i + 3] = byte.MaxValue; // gameboy doesn't handle opacity, so let's force it to max value.
+            }
+
+            uploadTex();
+        }
+
+        private void uploadTex()
+        {
+            var image = SixLabors.ImageSharp.Image.LoadPixelData<Rgba32>(screenData, 160, 144);
+            Texture.SetData(new TextureUpload(image));
+        }
+    }
+}
