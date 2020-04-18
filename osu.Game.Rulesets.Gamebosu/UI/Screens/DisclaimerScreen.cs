691b0ed74cc55d142c237070f342c89b7d2133fa
Lucas A <game4allyt@gmail.com>
2020-04-18 18:27:27 +0200

    Add text to disclaimer.

--- osu.Game.Rulesets.Gamebosu/UI/Screens/DisclaimerScreen.cs
+++ osu.Game.Rulesets.Gamebosu/UI/Screens/DisclaimerScreen.cs
@@ -1,5 +1,6 @@
 ﻿using osu.Framework.Allocation;
 using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
 
 namespace osu.Game.Rulesets.Gamebosu.UI.Screens
@@ -20,8 +21,23 @@ namespace osu.Game.Rulesets.Gamebosu.UI.Screens
         }
 
         [BackgroundDependencyLoader]
-        private void load()
+        private void load(OsuColour color)
         {
+            textFlow.AddIcon(FontAwesome.Solid.InfoCircle, t =>
+            {
+                t.Font = t.Font.With(size: 50);
+            });
+            textFlow.NewParagraph();
+            textFlow.AddParagraph("Disclaimer", delegate (SpriteText t)
+            {
+                t.Font = OsuFontExtensions.With(t.Font, Typeface.Torus, size: 30, weight: FontWeight.Bold);
+            });
+            textFlow.AddParagraph("This is a WIP, so don't expect things to work as expected.");
+            textFlow.AddParagraph("For now, please use beatmaps without breaks for the best experience!", delegate (SpriteText t)
+            {
+                t.Colour = color.Blue;
+            });
+            textFlow.NewParagraph();
         }
     }
 }
