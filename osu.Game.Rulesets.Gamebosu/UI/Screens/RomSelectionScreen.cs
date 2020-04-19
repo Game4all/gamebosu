f0c4291b0e97702f899d71a5bb146f7e2c97f2c2
Lucas A <game4allyt@gmail.com>
2020-04-19 22:43:32 +0200

    Add RomSelectionScreen.

--- osu.Game.Rulesets.Gamebosu/UI/Screens/RomSelectionScreen.cs
+++ osu.Game.Rulesets.Gamebosu/UI/Screens/RomSelectionScreen.cs
@@ -1,21 +1,21 @@
-﻿using osu.Framework.Graphics;
+﻿using Emux.GameBoy.Cartridge;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Logging;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Sprites;
+using osu.Game.Rulesets.Gamebosu.UI.Screens.Selection;
 
 namespace osu.Game.Rulesets.Gamebosu.UI.Screens
 {
     public class RomSelectionScreen : GamebosuScreen
     {
-        private FillFlowContainer flow;
-        private ScrollContainer<FillFlowContainer> scrolling;
-        private FillFlowContainer romFlow;
-
         public RomSelectionScreen()
         {
-            Child = flow = new FillFlowContainer
+            Child = new FillFlowContainer
             {
+                Margin = new MarginPadding { Top = 150 },
                 RelativeSizeAxes = Axes.Both,
                 Anchor = Anchor.TopCentre,
                 Origin = Anchor.TopCentre,
@@ -35,9 +35,17 @@ namespace osu.Game.Rulesets.Gamebosu.UI.Screens
                         Origin = Anchor.TopCentre,
                         Text = "Game selection",
                         Font = OsuFont.GetFont(Typeface.Torus, 32, FontWeight.Bold)
-                    }
+                    },
+                    new RomSelector()
+                    {
+                        RelativeSizeAxes = Axes.X,
+                        Height = 300,
+                        Selected = PushGameplay
+                    },
                 }
             };
         }
+
+        protected virtual void PushGameplay(EmulatedCartridge e) => Logger.Log($"Selected rom : {e.GameTitle}", LoggingTarget.Runtime, LogLevel.Debug);
     }
 }
---
+++ osu.Game.Rulesets.Gamebosu/UI/Screens/Selection/RomSelector.cs
@@ -0,0 +1,171 @@
+﻿using Emux.GameBoy.Cartridge;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.Input.Bindings;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Rulesets.Gamebosu.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace osu.Game.Rulesets.Gamebosu.UI.Screens.Selection
+{
+    public class RomSelector : CompositeDrawable, IKeyBindingHandler<GamebosuAction>
+    {
+        private IEnumerable<string> avalaible_roms;
+
+        public Action<EmulatedCartridge> Selected;
+
+        private readonly Sprite cartridge;
+
+        private RomStore roms;
+        private readonly OsuSpriteText romName;
+        private readonly SpriteIcon selectionLeft;
+        private readonly SpriteIcon selectionRight;
+
+        private BindableInt selection;
+
+        public RomSelector()
+        {
+            RelativeSizeAxes = Axes.Both;
+
+            InternalChild = new FillFlowContainer
+            {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                RelativeSizeAxes = Axes.Both,
+                Direction = FillDirection.Vertical,
+                Spacing = new osuTK.Vector2(0, 0.1f),
+                Children = new Drawable[]
+                {
+                    new Container
+                    {
+                        RelativeSizeAxes = Axes.X,
+                        Height = 300,
+                        Children = new Drawable[]
+                        {
+                            selectionLeft = new SpriteIcon
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                RelativePositionAxes = Axes.X,
+                                X = -0.25f,
+                                Size = new osuTK.Vector2(40),
+                                Icon = FontAwesome.Solid.ChevronLeft,
+                            },
+                            cartridge = new Sprite
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                Scale = new osuTK.Vector2(2f)
+                            },
+                            selectionRight = new SpriteIcon
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                RelativePositionAxes = Axes.X,
+                                X = 0.25f,
+                                Size = new osuTK.Vector2(40),
+                                Icon = FontAwesome.Solid.ChevronRight,
+                            },
+                        }
+                    },
+                    new Container
+                    {
+                        RelativeSizeAxes = Axes.X,
+                        AutoSizeAxes = Axes.Y,
+                        Child = romName = new OsuSpriteText
+                        {
+                            Font = OsuFont.GetFont(Typeface.Torus, 24, FontWeight.SemiBold),
+                            Text = "ROM NAME",
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                        }
+                    }
+                }
+            };
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(TextureStore store, RomStore roms)
+        {
+            this.roms = roms;
+
+            cartridge.Texture = store.Get("cartridge");
+            avalaible_roms = roms.GetAvailableResources();
+
+            selection = new BindableInt(0)
+            {
+                MinValue = 0,
+                MaxValue = ((avalaible_roms.Count() - 1) >= 0 ? (avalaible_roms.Count() - 1) : 0)
+            };
+
+            selection.BindValueChanged(updateSelection, true);
+        }
+
+        private void updateSelection(ValueChangedEvent<int> selection)
+        {
+            selectionLeft.FadeIn(200, Easing.OutQuint);
+            selectionRight.FadeIn(200, Easing.OutQuint);
+
+            if (selection.NewValue == this.selection.MaxValue)
+                selectionRight.FadeOut(200, Easing.OutQuint);
+
+            if (selection.NewValue == 0)
+                selectionLeft.FadeOut(200, Easing.OutQuint);
+
+            romName
+                .FadeOut(200, Easing.OutQuint)
+                .OnComplete(t =>
+                {
+                    t.Text = avalaible_roms.ElementAt(selection.NewValue);
+                    t.FadeIn(200, Easing.OutQuint);
+                });
+        }
+
+        private void setSelection(int idx)
+        {
+            try
+            {
+                selection.Value += idx;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public bool OnPressed(GamebosuAction action)
+        {
+            switch (action)
+            {
+                case GamebosuAction.DPadRight:
+                    setSelection(1);
+                    break;
+                case GamebosuAction.DPadLeft:
+                    setSelection(-1);
+                    break;
+
+                case GamebosuAction.ButtonA:
+                case GamebosuAction.ButtonStart:
+                case GamebosuAction.ButtonSelect:
+                    roms.GetAsync(avalaible_roms.ElementAt(selection.Value))
+                        .ContinueWith(t => Selected?.Invoke(t.Result));
+                    break;
+
+                default:
+                    break;
+            }
+
+            return true;
+        }
+
+        public void OnReleased(GamebosuAction action)
+        {
+        }
+    }
+}
