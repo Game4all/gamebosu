591e0f9d78f709f3c9d37b947eb29330bc69759f
Lucas A <game4allyt@gmail.com>
2020-04-29 12:47:08 +0200

    Use a span for gameboy screen screen data renderer.

--- osu.Game.Rulesets.Gamebosu/UI/Gameboy/DrawableGameboyScreen.cs
+++ osu.Game.Rulesets.Gamebosu/UI/Gameboy/DrawableGameboyScreen.cs
@@ -4,49 +4,44 @@
 using Emux.GameBoy.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
-using SixLabors.ImageSharp.PixelFormats;
+using System;
 
 namespace osu.Game.Rulesets.Gamebosu.UI.Gameboy
 {
     /// <summary>
     /// The drawable screen of a gameboy emulator.
     /// </summary>
-    //TODO: investigate whether it would be benefical to make this use a custom draw node instead of uploading textures every 1/60 of a second.
     public class DrawableGameboyScreen : Sprite, IVideoOutput
     {
-        private byte[] screenData;
+        private readonly SpanTextureUpload upload;
+        private Memory<byte> screenData;
 
         public DrawableGameboyScreen()
         {
             Texture = new Texture(160, 144);
-            screenData = new byte[160 * 144 * sizeof(int)]; //since the 4 components of a color (r, g, b a) are each a byte (4 bytes in total), the same as an int.
+            screenData = new Memory<byte>(new byte[160 * 144 * sizeof(int)]); //since the 4 components of a color (r, g, b a) are each a byte (4 bytes in total), the same as an int.
+            upload = new SpanTextureUpload(screenData);
         }
 
         public void Clear()
         {
             for (int i = 0; i < screenData.Length; i++)
-                screenData[i] = byte.MaxValue;
+                screenData.Span[i] = byte.MaxValue;
 
-            uploadTex();
+            Texture.SetData(upload);
         }
 
         public void RenderFrame(byte[] pixelData)
         {
             for (int i = 0, j = 0; j < pixelData.Length; i += 4, j += 3)
             {
-                screenData[i] = pixelData[j]; //r component
-                screenData[i + 1] = pixelData[j + 1]; //g component
-                screenData[i + 2] = pixelData[j + 2]; //b component
-                screenData[i + 3] = byte.MaxValue; // gameboy doesn't handle opacity, so let's force it to max value.
+                screenData.Span[i] = pixelData[j]; //r component
+                screenData.Span[i + 1] = pixelData[j + 1]; //g component
+                screenData.Span[i + 2] = pixelData[j + 2]; //b component
+                screenData.Span[i + 3] = byte.MaxValue; // gameboy doesn't handle opacity, so let's force it to max value.
             }
 
-            uploadTex();
-        }
-
-        private void uploadTex()
-        {
-            var image = SixLabors.ImageSharp.Image.LoadPixelData<Rgba32>(screenData, 160, 144);
-            Texture.SetData(new TextureUpload(image));
+            Texture.SetData(upload);
         }
     }
 }
---
+++ osu.Game.Rulesets.Gamebosu/UI/Gameboy/SpanTextureUpload.cs
@@ -0,0 +1,36 @@
+﻿using osu.Framework.Graphics.Primitives;
+using osu.Framework.Graphics.Textures;
+using osuTK.Graphics.ES30;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Runtime.InteropServices;
+
+namespace osu.Game.Rulesets.Gamebosu.UI.Gameboy
+{
+    public class SpanTextureUpload : ITextureUpload
+    {
+        private readonly Memory<byte> uploadData;
+
+        public SpanTextureUpload(Memory<byte> uploadData)
+        {
+            this.uploadData = uploadData;
+        }
+
+        public ReadOnlySpan<Rgba32> Data => MemoryMarshal.Cast<byte, Rgba32>(uploadData.Span);
+
+        public int Level => 0;
+
+        private RectangleI bounds = new RectangleI(0, 0, 160, 144);
+        public RectangleI Bounds
+        {
+            get => bounds;
+            set => bounds = value;
+        }
+
+        public PixelFormat Format => PixelFormat.Rgba;
+
+        public void Dispose()
+        {
+        }
+    }
+}
