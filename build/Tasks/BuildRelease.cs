0f09dbe26be1cf2225decdf33a4a495299021ad9
Lucas A <game4allyt@gmail.com>
2021-06-24 21:54:43 +0200

    Fix buildscript

--- build/Tasks/BuildRelease.cs
+++ build/Tasks/BuildRelease.cs
@@ -2,6 +2,7 @@ using Cake.Common.Diagnostics;
 using Cake.Common.Tools.DotNetCore;
 using Cake.Frosting;
 using Cake.Common.Tools.DotNetCore.MSBuild;
+using Cake.Common.Tools.DotNetCore.Build;
 
 [TaskName("BuildRelease")]
 [Dependency(typeof(RestoreProject))]
@@ -15,10 +16,15 @@ public sealed class BuildRelease : FrostingTask<Context>
 
         context.Information($"Building release version {context.ReleaseVersion}");
 
-        var settings = new DotNetCoreMSBuildSettings();
-        settings.SetConfiguration("Release");
-        settings.SetVersion(context.ReleaseVersion);
-        context.DotNetCoreMSBuild(context.RulesetProjectPath, settings);
+        var msbuildOpts = new DotNetCoreMSBuildSettings();
+        msbuildOpts.SetVersion(context.ReleaseVersion);
+
+        var buildOpts = new DotNetCoreBuildSettings {
+            Configuration = "Release",
+            MSBuildSettings = msbuildOpts
+        };
+
+        context.DotNetCoreBuild(context.RulesetProjectPath, buildOpts);
 
         context.Information("Release built sucessfully");
     }
