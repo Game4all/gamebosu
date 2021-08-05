10115d919471c34d925807d23e7fbdcd2e9e8da9
Lucas A <game4allyt@gmail.com>
2021-08-05 10:57:12 +0200

    Update lazer package version.

--- osu.Game.Rulesets.Gamebosu/UI/Configuration/GamebosuSettingsSubsection.cs
+++ osu.Game.Rulesets.Gamebosu/UI/Configuration/GamebosuSettingsSubsection.cs
@@ -5,6 +5,7 @@ using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics;
+using osu.Framework.Localisation;
 using osu.Framework.Platform;
 using osu.Framework.Screens;
 using osu.Game.Graphics;
@@ -26,7 +27,7 @@ namespace osu.Game.Rulesets.Gamebosu.UI.Configuration
         {
         }
 
-        protected override string Header => "gamebosu!";
+        protected override LocalisableString Header => "gamebosu!";
 
         [BackgroundDependencyLoader]
         private void load(Storage storage, DialogOverlay dialog, OsuGame game)
