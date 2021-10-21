9ec3f187bd7346f45320f117d0c888c4253befe3
Lucas A <game4allyt@gmail.com>
2021-10-21 10:27:51 +0200

    Relicense under legal name.

--- osu.Game.Rulesets.Gamebosu/UI/Gameboy/DrawableGameboy.cs
+++ osu.Game.Rulesets.Gamebosu/UI/Gameboy/DrawableGameboy.cs
@@ -1,4 +1,4 @@
-﻿// gamebosu! ruleset. Copyright Lucas A. aka Game4all. Licensed under GPLv3.
+﻿// gamebosu! ruleset. Copyright Lucas ARRIESSE aka Game4all. Licensed under GPLv3.
 // See LICENSE at root of repo for more information on licensing.
 
 using Emux.GameBoy;
