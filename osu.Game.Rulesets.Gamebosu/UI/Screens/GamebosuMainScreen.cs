4ce839c6894b6965a16b3fc4fb88ef234188c2fe
Lucas A <game4allyt@gmail.com>
2021-11-08 17:28:20 +0100

    Add lowpass filter to menus.

--- osu.Game.Rulesets.Gamebosu/UI/Screens/GamebosuMainScreen.cs
+++ osu.Game.Rulesets.Gamebosu/UI/Screens/GamebosuMainScreen.cs
@@ -2,9 +2,12 @@
 // See LICENSE at root of repo for more information on licensing.
 
 using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.Platform;
 using osu.Framework.Screens;
+using osu.Game.Audio.Effects;
 using osu.Game.Rulesets.Gamebosu.Configuration;
 using osu.Game.Rulesets.Gamebosu.IO;
 
@@ -18,6 +21,8 @@ namespace osu.Game.Rulesets.Gamebosu.UI.Screens
 
         private GamebosuConfigManager config;
 
+        private AudioFilter lowPassFilter;
+
         public GamebosuMainScreen(GamebosuRuleset ruleset)
         {
             this.ruleset = ruleset;
@@ -25,6 +30,12 @@ namespace osu.Game.Rulesets.Gamebosu.UI.Screens
             {
                 Child = screenStack = new GamebosuScreenStack()
             };
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(AudioManager audio)
+        {
+            lowPassFilter = new AudioFilter(audio.TrackMixer);
         }
 
         protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
@@ -43,6 +54,8 @@ namespace osu.Game.Rulesets.Gamebosu.UI.Screens
 
         public override void OnEntering(IScreen last)
         {
+            lowPassFilter.CutoffTo(1000, 1200, Easing.OutQuint);
+
             var displayDisclaimer = !config.Get<bool>(GamebosuSetting.DisableDisplayingThatAnnoyingDisclaimer);
             screenStack.Push(displayDisclaimer ? new DisclaimerSubScreen
             {
@@ -54,6 +67,7 @@ namespace osu.Game.Rulesets.Gamebosu.UI.Screens
 
         public override bool OnExiting(IScreen next)
         {
+            lowPassFilter.CutoffTo(AudioFilter.MAX_LOWPASS_CUTOFF, 300);
             if (screenStack.CurrentScreen is GameplaySubScreen)
             {
                 screenStack.Exit();
